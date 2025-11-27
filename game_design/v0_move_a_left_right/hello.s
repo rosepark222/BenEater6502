a10876c21fceb657cd7b43db608c37507385831a
Andrew Park <andrew.k.park7@gmail.com>
2025-11-26 19:58:08 -0800

    moving hero left and right; display its position at the right most of 2nd column; print scan code at the second row

--- game_design/v0_move_a_left_right/hello.s
+++ game_design/v0_move_a_left_right/hello.s
@@ -22,12 +22,16 @@ PS2_REPLY_FF = $FF
 
 ; Simple game variables
 CHAR_POS     = $0200      ; Position of 'a' character (0-15)
-SCAN_ROW_POS = $0201      ; Current position in second row for scancode display (0-15)
+SCAN_ROW_POS = $0201      ; Current position in second row for scancode display (0-13, reserve 14-15 for position)
+TIMER_COUNT  = $0202      ; Timer counter for 5 second clear (3 bytes for larger count)
+TIMER_COUNT_MID = $0203
+TIMER_COUNT_HI = $0204
 
 ; Keyboard variables
 HANDSHAKE_DONE = $0222
 PS2_BYTE_TEMP  = $0223
 CURRENT_SCANCODE = $0224  ; Store the scancode received
+BREAK_CODE_FLAG = $0225   ; Flag to indicate F0 (break code) was received
 
 ; PS/2 Scan codes for h and l
 SCANCODE_H     = $33  ; h - left
@@ -48,6 +52,7 @@ keyboard_init:
   LDA #$00
   STA HANDSHAKE_DONE
   STA CURRENT_SCANCODE
+  STA BREAK_CODE_FLAG
 
 lcd_init:
   lda #%11111111
@@ -112,28 +117,129 @@ game_init:
   STA CHAR_POS
   STA SCAN_ROW_POS
   
+  ; Initialize timer for 5 second clear (5,000,000 cycles at 1MHz)
+  ; Store as 24-bit value: $4C4B40
+  LDA #$40
+  STA TIMER_COUNT
+  LDA #$4B
+  STA TIMER_COUNT_MID
+  LDA #$4C
+  STA TIMER_COUNT_HI
+  
   ; Display 'a' at first position
-  JSR display_frame
+  JSR display_hero
 
 main_loop:
-  ; Just wait for interrupts
+  ; Decrement timer
+  LDA TIMER_COUNT
+  BNE dec_low
+  LDA TIMER_COUNT_MID
+  BNE dec_mid
+  LDA TIMER_COUNT_HI
+  BNE dec_high
+  
+  ; Timer reached zero - clear screen and reset
+  JSR clear_screen_reset
   JMP main_loop
 
-; Display the current frame
-display_frame:
+dec_low:
+  DEC TIMER_COUNT
+  JMP main_loop
+
+dec_mid:
+  DEC TIMER_COUNT_MID
+  LDA #$FF
+  STA TIMER_COUNT
+  JMP main_loop
+
+dec_high:
+  DEC TIMER_COUNT_HI
+  LDA #$FF
+  STA TIMER_COUNT_MID
+  STA TIMER_COUNT
+  JMP main_loop
+
+clear_screen_reset:
   ; Clear display
   LDA #%00000001
   JSR lcd_instruction
   
+  ; Reset scancode position
+  LDA #0
+  STA SCAN_ROW_POS
+  
+  ; Reset timer
+  LDA #$40
+  STA TIMER_COUNT
+  LDA #$4B
+  STA TIMER_COUNT_MID
+  LDA #$4C
+  STA TIMER_COUNT_HI
+  
+  ; Redisplay 'a'
+  JSR display_hero
+  RTS
+
+remove_hero:
+  LDA CHAR_POS 
+  ORA #%10000000  
+  JSR lcd_instruction
+  LDA #' '
+  JSR print_char
+  RTS
+ 
+display_hero:
+  ; Clear display
+  ; for some reason, it clears the screen and below display did not work
+  ; instead, remove previous 'a' and display the current 'a' worked
+;  LDA #%00000001
+;  JSR lcd_instruction
+  
   ; Set cursor to first row, CHAR_POS position
-  LDA #%10000000  ; First row base address
-  ORA CHAR_POS
+  LDA CHAR_POS    ; Load position first
+  ORA #%10000000  ; Then OR with first row base address
   JSR lcd_instruction
   
   ; Display 'a'
   LDA #'a'
   JSR print_char
   
+  ; Display position at column 14-15 of second row
+  ; JSR display_position
+  
+  RTS
+
+; Display row and column position at second row columns 14-15
+display_position:
+  ; Set cursor to second row, column 14
+  LDA #%11001110  ; Second row base + 14
+  JSR lcd_instruction
+  
+  ; Display row (always 0 since we only have first row for 'a')
+  LDA #'0'
+  JSR print_char
+  
+  ; Set cursor to second row, column 15
+  LDA #%11001111  ; Second row base + 15
+  JSR lcd_instruction
+  
+  ; Display column position (convert CHAR_POS to ASCII digit)
+  LDA CHAR_POS
+  CMP #10
+  BCC single_digit
+  
+  ; For positions 10-15, display A-F
+  SEC
+  SBC #10
+  CLC
+  ADC #'A'
+  JSR print_char
+  RTS
+  
+single_digit:
+  CLC
+  ADC #'0'
+  JSR print_char
   RTS
 
 ; Print character in A to LCD
@@ -153,8 +259,8 @@ print_scancode_hex:
   PHA  ; Save original scancode
   
   ; Set cursor to second row at SCAN_ROW_POS
-  LDA #%11000000  ; Second row base address
-  ORA SCAN_ROW_POS
+  LDA SCAN_ROW_POS ; Load position first
+  ORA #%11000000   ; Then OR with second row base address
   JSR lcd_instruction
   
   ; Print high nibble
@@ -174,15 +280,15 @@ high_digit:
   ; Increment position
   INC SCAN_ROW_POS
   LDA SCAN_ROW_POS
-  CMP #16
+  CMP #14         ; Stop at column 13 (reserve 14-15 for position display)
   BCC print_low_nibble
   LDA #0
   STA SCAN_ROW_POS
   
 print_low_nibble:
   ; Set cursor again for low nibble
-  LDA #%11000000
-  ORA SCAN_ROW_POS
+  LDA SCAN_ROW_POS ; Load position first
+  ORA #%11000000   ; Then OR with second row base address
   JSR lcd_instruction
   
   ; Print low nibble
@@ -199,7 +305,7 @@ low_digit:
   ; Increment position
   INC SCAN_ROW_POS
   LDA SCAN_ROW_POS
-  CMP #16
+  CMP #14         ; Stop at column 13 (reserve 14-15 for position display)
   BCC done_print
   LDA #0
   STA SCAN_ROW_POS
@@ -252,35 +358,76 @@ IRQ_HANDLER:
   
   JMP CLEAR_IRQ
 
+; PROCESS_SCANCODE:
+;   ; Print all scancodes to second row
+;   LDA CURRENT_SCANCODE
+;   JSR print_scancode_hex
+  
+;   ; Check if it's 'h' (move left)
+;   LDA CURRENT_SCANCODE
+;   CMP #SCANCODE_H
+;   BEQ move_left
+  
+;   ; Check if it's 'l' (move right)
+;   CMP #SCANCODE_L
+;   BEQ move_right
+  
+;   JMP CLEAR_IRQ
+
+
 PROCESS_SCANCODE:
-  ; Print all scancodes to second row
+
+  LDA CURRENT_SCANCODE
+  ; -------------------------------
+  ; Check BREAK_CODE_FLAG
+  ; -------------------------------
+  LDX BREAK_CODE_FLAG
+  BEQ NOT_IN_BREAK
+; ===============================
+; FLAG SET: this is the key released
+; ===============================
+IN_BREAK:
+  LDA #0
+  STA BREAK_CODE_FLAG        ; Clear flag
+  ; Display released scancode
   LDA CURRENT_SCANCODE
   JSR print_scancode_hex
-  
-  ; Check if it's 'h' (move left)
-  LDA CURRENT_SCANCODE
+  ; Move left?
   CMP #SCANCODE_H
   BEQ move_left
-  
-  ; Check if it's 'l' (move right)
+  ; Move right?
   CMP #SCANCODE_L
   BEQ move_right
-  
+
+  JMP CLEAR_IRQ
+; ===============================
+; FLAG NOT SET: check for F0
+; ===============================
+NOT_IN_BREAK:
+  CMP #$F0
+  BNE CLEAR_IRQ              ; Not F0, ignore and return
+  ; If F0 received, set break flag
+  LDA #$01
+  STA BREAK_CODE_FLAG
   JMP CLEAR_IRQ
 
 move_left:
   LDA CHAR_POS
   BEQ CLEAR_IRQ  ; Already at leftmost position
+  jsr remove_hero
   DEC CHAR_POS
-  JSR display_frame
+  JSR display_hero
+  jsr display_position
   JMP CLEAR_IRQ
 
 move_right:
   LDA CHAR_POS
   CMP #15
   BEQ CLEAR_IRQ  ; Already at rightmost position
+  jsr remove_hero
   INC CHAR_POS
-  JSR display_frame
+  JSR display_hero
+  jsr display_position
   JMP CLEAR_IRQ
 
 CLEAR_IRQ:
@@ -429,4 +576,3 @@ lcd_long_delay_loop:
   .word $0000
   .word reset
   .word IRQ_HANDLER
-
